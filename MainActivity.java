1ba231cbe50ee2b9a6a5805218331d60537594a9
Zubir93 <52188553+Zubir93@users.noreply.github.com>
2019-06-26 11:01:45 +0800

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,36 +1,87 @@
-package com.bitm3453.myquiz;
+package com.example.myquiz;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
-import android.content.pm.LabeledIntent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_QUIZ = 1;
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String KEY_HIGHSCORE = "keyHighscore";
+
+    private TextView textViewHighscore;
+
+    private int highscore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-///////////////////////BUTTON///////////////////////////////////////
+        //////////////WEB SERVICES////////////////
+
+        //////////////WEB SERVICES////////////////
+
+        textViewHighscore = findViewById(R.id.text_view_highscore);
+        loadHighscore();
+
+
         Button buttonStartQuiz = findViewById(R.id.button_start_quiz);
         buttonStartQuiz.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-///////////////////////CALL METHOD///////////////////////////////////////
-                  startQuiz();
-///////////////////////CALL METHOD///////////////////////////////////////
+                startQuiz();
+
+                //notification
+                NotificationManager notif = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                Notification notify = new Notification.Builder(getApplicationContext()).setContentTitle("Mula quiz").setSmallIcon(R.drawable.ic_launcher_foreground).build();
+                notify.flags |= Notification.FLAG_AUTO_CANCEL;
+                notif.notify(0, notify);
             }
         });
     }
 
-///////////////////////CALL METHOD//////////////////////////////////////
     private void startQuiz() {
-        //////ERROR/////Intent intent =new Intent(packageContext: StartingScreenActivity.this,QuizActivity.class);
-       //// startActivity(Intent);
+        Intent intent = new Intent(MainActivity.this, QuizActivity.class);
+        startActivityForResult(intent, REQUEST_CODE_QUIZ);
     }
-///////////////////////CALL METHOD//////////////////////////////////////
-}
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_QUIZ) {
+            if (resultCode == RESULT_OK) {
+                int score = data.getIntExtra(QuizActivity.EXTRA_SCORE, 0);
+                if (score > highscore) {
+                    updateHighscore(score);
+                }
+            }
+        }
+    }
+
+    private void loadHighscore() {
+        SharedPreferences prefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        highscore = prefs.getInt(KEY_HIGHSCORE, 0);
+        textViewHighscore.setText("Markah Terkumpul : " + highscore);
+    }
+
+    private void updateHighscore(int highscoreNew) {
+        highscore = highscoreNew;
+        textViewHighscore.setText("Markah Terkumpul : " + highscore);
+
+        SharedPreferences prefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(KEY_HIGHSCORE, highscore);
+        editor.apply();
+    }
+}
